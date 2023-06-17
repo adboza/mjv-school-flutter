f9a58ef7690e374d55a8d843070cb337fc748486
Adriana Boza <adboza@gmail.com>
2023-06-16 21:21:07 -0300

    aula 5 incompleta

--- mjvproject/lib/pages/home/abas/afazeres_tab.dart
+++ mjvproject/lib/pages/home/abas/afazeres_tab.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mjvproject/components/icon_button_component.dart';
+import 'package:mjvproject/components/spacer_component.dart';
 
-import '../../../components/spacer_component.dart';
+import '../../../entities/afazer_entity.dart';
 
 class AfazeresTab extends StatefulWidget {
-  final int valorIniciaL;
-  final void Function(int tabIndx)? callback;
-
   const AfazeresTab({
     super.key,
-    required this.valorIniciaL,
-    this.callback,
   });
 
   @override
@@ -17,23 +14,44 @@ class AfazeresTab extends StatefulWidget {
 }
 
 class _AfazeresTab extends State<AfazeresTab> {
-  late int acumulador;
+  late List<AfazerEntity> _listaAfazeres;
 
-  void somarMaiUm() {
+  void handleAdicionar() {
     setState(() {
-      acumulador++;
+      _listaAfazeres.add(
+        AfazerEntity(
+            uuid: 'teste1',
+            titulo: 'teste 1',
+            dataFim: DateTime.now(),
+            dataInicio: DateTime.now(),
+            isConcluido: false),
+      );
     });
   }
 
-  void handleCallback() {
-    if (widget.callback != null) {
-      widget.callback!(1);
-    }
+  void handleExcluir(int index){
+    _listaAfazeres.removeAt(index);
+    setState(() {
+      _listaAfazeres = _listaAfazeres;
+    });
   }
 
   @override
   void initState() {
-    acumulador = widget.valorIniciaL;
+    _listaAfazeres = [
+      AfazerEntity(
+          uuid: 'teste1',
+          titulo: 'teste 1',
+          dataFim: DateTime.now(),
+          dataInicio: DateTime.now(),
+          isConcluido: false),
+      AfazerEntity(
+          uuid: 'teste2',
+          titulo: 'teste 2',
+          dataFim: DateTime.now(),
+          dataInicio: DateTime.now(),
+          isConcluido: true),
+    ];
     super.initState();
   }
 
@@ -41,16 +59,27 @@ class _AfazeresTab extends State<AfazeresTab> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('$acumulador'),
-        const SpacerComponent(),
         ElevatedButton(
-          onPressed: somarMaiUm,
-          child: const Text('+1'),
+          onPressed: handleAdicionar,
+          child: const Text('Adicionar'),
         ),
-        const SpacerComponent(),
-        ElevatedButton(
-          onPressed: handleCallback,
-          child: const Text('callback'),
+        SizedBox(
+          width: double.infinity,
+          height: 400,
+          child: ListView.builder(
+            itemCount: _listaAfazeres.length,
+            itemBuilder: (context, index) {
+              final item = _listaAfazeres.elementAt(index);
+              return Dismissible(key: Key(item.uuid), child: Text(item.titulo));
+              // return Row(
+              //   children: [
+              //     Text(item.titulo),
+              //     const SpacerComponent(isHorizontal: true),
+              //     IconButtonComponent(onPressed: () => handleExcluir(index)),
+              //   ],
+              // );
+            },
+          ),
         ),
       ],
     );
